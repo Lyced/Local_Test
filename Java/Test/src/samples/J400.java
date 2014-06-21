190ea228eb92a32f553bfea201158017c7b90303
Lyced <betaep1027@gmail.com>
2014-06-21 01:27:52 -0400

    J400.v1
    Version 1 of the bank checking program, updated to read from a text file
    rather than a prompt for user input.
    Test text file included.

--- Java/Test/src/samples/J400.java
+++ Java/Test/src/samples/J400.java
@@ -1,71 +1,170 @@
 package samples;
 
 import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Stack;
 
 public class J400 {
 	
-	public static void main(String []arg) {
+	public static void main(String []arg) throws IOException{
 		
 		int numBanks, i, loanCount;
 		double limit;
 		Bank[] bankList;
-		Scanner input = new Scanner(System.in);
+		Stack<Integer> unsafeBanks = new Stack<Integer>();
+		String filepath;
+		Scanner fileinput = new Scanner(System.in);
+		Scanner input = null;
 		
-		System.out.print("Please enter the number of banks and the limit: ");
-		numBanks = input.nextInt();
-		limit = input.nextDouble();
-		
-		bankList = new Bank[numBanks];
-		
+		try {
+			
+			System.out.print("Please enter the location of the bank data file: ");
+			filepath = fileinput.nextLine();
+			
+			input = new Scanner(new BufferedReader (new FileReader (filepath)));
+			
+			numBanks = input.nextInt();
+			limit = input.nextDouble();
+			
+			bankList = new Bank[numBanks];
+			
+			for (i = 0; i < numBanks; i++) {
+			
+				bankList[i] = new Bank(); 
+				
+			//	System.out.print("Please enter the bank balance: ");
+				bankList[i].baseBalance = input.nextDouble();
+								
+			//	System.out.print("Please enter the number of loans this bank gave out: ");
+				bankList[i].numLoans = input.nextInt();
+				
+				bankList[i].initializeLoanList();
+				
+				for (loanCount = 0; loanCount < bankList[i].numLoans; loanCount++) {
+					
+			//		System.out.print("Please enter the bank and the loaned amount: ");
+					bankList[i].loanList[loanCount].loanee = input.nextInt();
+					bankList[i].loanList[loanCount].amount = input.nextDouble();
+					
+				}
+			} 
+			
+		} finally {
+				
+				if (input != null) {
+					input.close();
+				}
+				
+				if (fileinput != null) {
+					fileinput.close();
+				}
+		}
+		/*	
 		for (i = 0; i < numBanks; i++) {
+				
+			System.out.println("The bankID is: " + i);
+			bankList[i].showData();
+																							
+		}
+		*/
+		unsafeBanks = findUnsafe(bankList, limit, unsafeBanks);
 		
-			bankList[i] = new Bank(); 
+		System.out.print("The following banks are unsafe: ");
+
+		while (!unsafeBanks.empty()) {
 			
-			System.out.print("Please enter the bank balance: ");
-			bankList[i].balance = input.nextDouble();
+			System.out.print(unsafeBanks.pop() + " ");
 			
-			System.out.print("Please enter the number of loans this bank gave out: ");
-			bankList[i].numLoans = input.nextInt();
+		}
+		
+		System.out.println(".");
 			
-			bankList[i].initializeLoanList();
-			
-			for (loanCount = 0; loanCount < bankList[i].numLoans; loanCount++) {
+	}
+
+	public static Stack<Integer> findUnsafe(Bank[] bankList, double limit, Stack<Integer> unsafeBanks) {
+		
+		int c, l;
+		boolean stateChanged = true;
+		
+		while (stateChanged) {
+		
+			for (c = 0; c < bankList.length; c++) {
 				
-				System.out.print("Please enter the bank and the loaned amount: ");
-				bankList[i].loanList[loanCount].loanee = input.nextInt();
-				bankList[i].loanList[loanCount].amount = input.nextDouble();
+				if (bankList[c].unsafe == false) {
 				
+					bankList[c].currentBalance = bankList[c].baseBalance;
+					
+					for (l = 0; l <bankList[c].loanList.length; l++) {
+					
+						bankList[c].currentBalance += bankList[c].loanList[l].amount;
+						
+					}
+					
+					System.out.println("Bank " + c + " current balance is " + bankList[c].currentBalance + ".");
+					
+					if (bankList[c].currentBalance < limit) {
+						
+						unsafeBanks.push(c);
+						bankList[c].unsafe = true;
+						stateChanged = true;
+						updateUnsafe(bankList, c);
+						break;
+						
+					} else {
+						
+						stateChanged = false;
+						
+					}
+				}
 			}
 		}
 		
-		for (i = 0; i < numBanks; i++) {
-			
-			System.out.println("The bankID is: " + i);
-			bankList[i].showData();
-			
-		}
-			
-		input.close();
+		return unsafeBanks;
 	}
-
+	
+	public static void updateUnsafe(Bank[] bankList, int c) {
+		
+		int u, ul;
+		
+		for (u = 0; u < bankList.length; u++) {
+			
+			for (ul = 0; ul < bankList[u].loanList.length; ul++) {
+			
+				if (bankList[u].loanList[ul].loanee == c) {
+					
+					System.out.println("Bank " + u +"'s loan to " + bankList[u].loanList[ul].loanee + " for amount " + bankList[u].loanList[ul].amount + " has been invalidated.");
+					
+					bankList[u].loanList[ul].amount = 0;
+					
+				}
+			
+			}
+		}
+	}
+	
 	private static class Bank {
 		
 		int numLoans, l;
-		double balance;
-		loanee[] loanList;  //First array contains the bankID of the loanee and the second one is of the amount loaned.
+		double baseBalance, currentBalance;
+		boolean unsafe;
+		Loanee[] loanList;  //First array contains the bankID of the loanee and the second one is of the amount loaned.
 		
 		public Bank(){
 			numLoans = 0;
-			balance = 0;
+			baseBalance = 0;
+			currentBalance = 0;
+			unsafe = false;
 		}
 			
 		public void initializeLoanList () {
 			
-			loanList = new loanee[numLoans];
+			loanList = new Loanee[numLoans];
 			
 			for (l = 0; l< numLoans; l++) {
 				
-				loanList[l] = new loanee();
+				loanList[l] = new Loanee();
 				
 				loanList[l].loanee = 0;
 				loanList[l].amount = 0;
@@ -75,26 +174,27 @@ public class J400 {
 		
 		public void showData() {
 			
-			System.out.println("The bank balance is: " + balance);
+			System.out.println("The bank balance is: " + currentBalance);
 			System.out.println("The bank gave out " + numLoans + " loans.");
 			
 			for (l = 0; l < numLoans; l++) {
 				
 				System.out.println("Loan " + (l + 1) + " is bank " + loanList[l].loanee + " with a loan amount of $" + loanList[l].amount + ".");
-				System.out.println();
+				
 			}
 			
+			System.out.println();
 		}
 		
 	}
 	
 	
-	private static class loanee {
+	private static class Loanee {
 		
 		int loanee;
 		double amount;
 		
-		public loanee() {
+		public Loanee() {
 			
 			loanee = 0;
 			amount = 0;
--- Java/Test/src/samples/filereadtest.java
+++ Java/Test/src/samples/filereadtest.java
@@ -1,28 +1,69 @@
 package samples;
 
-import java.io.FileReader;
-import java.io.File;
 import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class filereadtest {
 
-	public static void main (String []args) {
+	/*
+	public static void main (String []args) throws IOException {
 		
-		int i;
-		String fileLocation;
+	//	int i;
+		String filecontent;
 		Scanner input = new Scanner(System.in);
-		File file = new File(fileLocation);
-		FileReader filein = new FileReader(file);
+	//	File file = new File(fileLocation);
+	//	FileReader filein = null;
+		BufferedReader BReader = null;
+		Path filepath = Paths.get("D:\\Dropbox\\BankData.txt");
 		
-		System.out.print("Please enter the filename location: ");
-		fileLocation = input.nextLine();
-		file.
-		
-		while (fileLocation != "End") {
+		try {
 			
-			System.out.println(filein.read());
+			//filein = new FileReader(filepath);
+			BReader = Files.newBufferedReader(filepath);			
+		
+	//	System.out.print("Please enter the filename location: ");
+	//	fileLocation = input.nextLine();
+	//	filein.read();
+		
+			while ((filecontent = BReader.readLine()) != null) {
+				
+				System.out.println(filecontent);
+				
+			}
+		} finally {
+			
+			if (BReader != null) {
+				BReader.close();
+			}
 			
 		}
+	}
+	*/
+	public static void main (String []args) throws IOException {
 		
+		double temp;
+		String Location;
+		Scanner input = null;
+		Path filepath = Paths.get("D:\\Dropbox\\BankData.txt");
+		
+		try {
+			Location = filepath.toString();
+			input = new Scanner(new BufferedReader (new FileReader (Location)));
+			
+			while (input.hasNext()) {
+				
+				temp = input.nextDouble();
+				System.out.println(temp);
+			}
+		} finally {
+			if (input != null) {
+				input.close();
+			}
+		}
 	}
 }
